a5cb3fe0923693fb0daec2cd9aa50b057f291ebc
Dementev_Artem <artemdementev561@gmail.com>
2021-10-08 10:19:40 +0300

    Create a new project

---
+++ GenerationOfCombinations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenerationOfCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
