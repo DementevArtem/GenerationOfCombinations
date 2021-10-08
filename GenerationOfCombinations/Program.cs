31e503eb89853cfda80d9fc96eaee77ed837d6b9
Dementev_Artem <artemdementev561@gmail.com>
2021-10-09 02:24:16 +0300

    Created a good algorithm

--- GenerationOfCombinations/Program.cs
+++ GenerationOfCombinations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenerationOfCombinations
 {
@@ -6,7 +7,27 @@ namespace GenerationOfCombinations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int j = 0;
+            var array = new List<int> { 0, 1, 2 };
+            array.Add(5);
+            array.Add(0);
+
+            while(true)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+                for (j = 0; j < array.Count-1; j++)
+                {
+                    if (array[j] + 1 == array[j + 1])
+                        array[j] = j;
+                    else break;
+                }
+                if (j < 3) array[j] += 1;
+                else break;
+            }
         }
     }
 }
